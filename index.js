60d6d2ffaab5b64f4e3c330a038175a6b5b733eb
khalid-zein <zeinkhalid004@gmail.com>
2022-11-22 21:45:23 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,48 @@
+function add(a, b){
+    let result = a + b;
+
+    return result;
+};
+console.log(result)
+function subtract(a, b){
+    let result = a - b;
+
+    return result;
+};
+console.log(result);
+function multiply(a, b){
+    let result = a * b;
+
+    return result;
+};
+console.log(result);
+function divide(a,b){
+    let result = a / b;
+
+    return result
+};
+console.log(result);
+
+function increment(a){
+    a += 1
+
+    return a;
+}
+console.log(a);
+
+function decrement(a){
+    a -= 1
+
+    return a;
+}
+console.log(a);
+
+function makeInt(a){
+    return parseInt(a, 10);
+}
+console.log(a.toString());
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+console.log(preserveDecimal(2.222));
